16a11b4cdf39b44e39ed8103a78fd493e636ffe4
Luoana Chirita <luoana.livadariu@gmail.com>
2018-09-01 11:40:25 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,32 @@
+class BoardMember {
+  constructor(name, homeState, training) {
+    this.name = name
+    this.homeState = homeState
+    this.training = training
+  }
+  
+  veto() {
+    console.log("No, I must disagree")
+  }
+  approve() {
+    console.log("You can do that!")
+  }
+  doCharity() {
+    console.log("I like to help people.")
+  }
+  releasePressStatement() {
+    console.log("You will see great things from Scuber.")
+  }
+  sayHi() {
+    console.log(`Hi, my name is ${this.name}. I am from ${this.homeState}, and I was trained in ${this.training}.`)
+  }
+}
+
+class Ceo extends BoardMember {
+  constructor(name, training, homeState) {
+    super.constructor(name, training, homeState)
+  }
+  hireEmployee() {
+    console.log("Welcome aboard!")
+  }
+}
